3340e1d364920d94be01fc2341ee2123b7c2c442
Devin Durbin <35539466+ddurbin1@users.noreply.github.com>
2020-03-05 17:09:10 -0600

    Update main_game.py
    The beginning of the game.

--- main_game.py
+++ main_game.py
@@ -1,2 +1,31 @@
 """ This file houses the main code of the program that will primarily call from other files. """
 
+class character:
+    def Name():
+        global name
+        name = input("What's your name? ")
+        print("Hello {}".format(name))
+
+    def Race():
+        print("Race Types:")
+        print("Human")
+        print("Reptilian")
+        print("Barbarian")
+        print("Blob")
+        global race
+        race = input("What's your race? ")
+
+class game:
+    def start():
+        print("Welcome to the game. It's a work in progress so don't be surprised if it's bad.")
+        character.Name()
+        character.Race()
+
+    def home():
+        if race.lower() == 'human':
+            print("You are awoken by the sounds of war drums, pounding away.")
+        else:
+            print("You awake in the woods.")
+
+game.start()
+game.home()
