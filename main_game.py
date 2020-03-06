777d04c869b29a8dcca21e27da843acfa663e718
Devin Durbin <35539466+ddurbin1@users.noreply.github.com>
2020-03-05 20:33:17 -0600

    Update to main_game.py
    Creation of dominant check, inventory, location functions. also the beginning of the first conflict.

--- main_game.py
+++ main_game.py
@@ -13,19 +13,63 @@ class character:
         print("Barbarian")
         print("Blob")
         global race
-        race = input("What's your race? ")
+        race = input("What's your race? ").lower()
+
+    def Dominant():
+        global dom
+        dom = input("Are you left handed or right? ")
+
+    def Inventory():
+        global inv, head, left_arm, right_arm, torso, legs, feet
+        
+        inv = {
+          "head": "None",
+          "left_arm": "None",
+          "right_arm": "None",
+          "torso": "None",
+          "legs": "None",
+          "feet": "None"
+        }
+
+    def Location():
+        global local, main, level
+        local = []
+        main = local[0]
+        level = local[1]
 
 class game:
+    def input():
+        user = input("")
+    
     def start():
         print("Welcome to the game. It's a work in progress so don't be surprised if it's bad.")
         character.Name()
         character.Race()
+        character.Dominant()
 
     def home():
-        if race.lower() == 'human':
+        if race == 'human':
             print("You are awoken by the sounds of war drums, pounding away.")
         else:
             print("You awake in the woods.")
 
+    def first_comflict():
+        if race == 'human':
+            print("You get up and quickly throw on some basic armor. You reach for sword and march to the main living area.")
+            character.Inventory()
+            inv["torso"] = "Basic Armor"
+            inv["legs"] = "Basic Armor"
+            inv["feet"] = "Boots"
+            if dom == 'right':
+                inv["right_arm"] = "Basic Sword"
+            elif dom == 'left':
+                inv["left_arm"] = "Basic Sword"
+            print("Inventory updated...")
+            print(inv)
+        else:
+            print("test")
+            
+
 game.start()
 game.home()
+game.first_comflict()
